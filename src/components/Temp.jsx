fc8ffbd975391aa9ddf84e0e1c3218ba55ab848f
Bex Birkett <bex.birkett@outlook.com>
2026-02-04 17:27:56 +0000

    Updated colors and layout for Temp component

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -1,4 +1,6 @@
 // Temp Component
 export default function Temp() {
-  return <div></div>;
+  return (
+    <div className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden bg-amethystlight"></div>
+  );
 }
