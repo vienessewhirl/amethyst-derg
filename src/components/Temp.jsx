76111bbea4d64d8aada9544e9c50ff1967f601a8
Bex Birkett <bex.birkett@outlook.com>
2026-02-04 19:34:12 +0000

    Logo adjustment, font implemented, code cleanup

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -1,8 +1,24 @@
 // Temp Component
 export default function Temp() {
   return (
-    <section className="relative min-h-screen pt-16 sm:pt-20 px-0 overflow-hidden bg-amethystlight">
-      <div className="absolute left-1/2 -translate-x-1/2 top-1/4 w-[200%] aspect-square rounded-full bg-amethystdark"></div>
+    // Background section
+    <section className="min-h-screen overflow-hidden bg-amethystlight">
+      {/* Logo and Amethyst text */}
+      <div className="flex items-center justify-center relative z-20">
+        <p
+          className="text-white text-8xl tracking-widest"
+          style={{ fontFamily: "TikTok Font" }}
+        >
+          AMETHYST
+        </p>
+        <img
+          src="/logo_base.png"
+          alt="Amethyst Derg holding a sewing needle"
+          className="w-1/4 h-1/4 p-2"
+        ></img>
+      </div>
+      {/* Circle background */}
+      <div className="absolute left-1/2 -translate-x-1/2 top-1/4 w-[200%] aspect-square rounded-full bg-amethystdark border-20 border-white z-10"></div>
     </section>
   );
 }
