738d2923fc7f8662dcc514c256483a5250e30484
vienessewhirl <rebeccabirkett@outlook.com>
2026-02-07 09:53:11 +0000

    Tailwind CSS integration and base path configuration for Vite.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        amethystlight: "#d7cff5",
+        amethystdark: "#9389bc",
+      },
+    },
+  },
+};
